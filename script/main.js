829f359664882957c1ef0f0824295e0b6c3db391
Mantas-Vilimas <mantass.vilimas@gmail.com>
2023-04-11 12:03:34 +0300

    changed skills descriptions

--- script/main.js
+++ script/main.js
@@ -5,6 +5,12 @@ const header = document.querySelector("header");
 
 let menuOpen = false;
 
+const alert = () => {
+  alert(
+    "Currently working on this project. Sorry that you have to see unfinished product"
+  );
+};
+
 burgerBtn.addEventListener("click", function () {
   if (!menuOpen) {
     burgerBtn.classList.add("open");
